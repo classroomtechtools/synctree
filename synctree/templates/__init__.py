76a644481b9c484b51ccdcc34856a94b7bb7296c
Adam Morris <amorris@mistermorris.com>
2017-02-04 21:05:06 +0800

    reorganized templates into folder

---
+++ synctree/templates/__init__.py
@@ -0,0 +1,5 @@
+from .default_template import DefaultTemplate
+from .logger_template import LoggerTemplate
+from .logger_template import LoggerReporter
+
+__all__ = [DefaultTemplate, LoggerTemplate, LoggerReporter]
---
+++ synctree/templates/logger_template.py
@@ -0,0 +1,40 @@
+from . import DefaultTemplate
+from collections import DefaultDict
+
+
+class LoggerReporter:
+    """
+    Keep records of everything that has been done
+    """
+    _log = defaultdict(lambda: defaultdict(list))
+
+    def append_this(self, action, obj):
+        self._log[action.obj.__subbranch__][action.idnumber].append(obj)
+
+    def exception(self, action, result):
+        self._log[action.obj.__subbranch__][action.idnumber].append((action, result))
+
+    def success(self, action, result):
+        self._log[action.obj.__subbranch__][action.idnumber].append((action, result))
+
+    def fail(self, action, result):
+        self._log[action.obj.__subbranch__][action.idnumber].append((action, result))
+
+    def will_start(self):
+        pass
+
+    def finished(self):
+        pass
+
+    def not_implemented(self, action, result):
+        """
+        Override if this
+        """
+        # FIXME: Where to store the context info?
+        self._log['unimplemented'][result.method].append(result)
+
+
+class LoggerTemplate(DefaultTemplate):
+    """
+    """
+    _reporter_class = LoggerReporter
