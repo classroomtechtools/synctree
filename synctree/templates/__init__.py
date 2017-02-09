403d154312b51b68656e5a361f4d30855d472c4c
Adam Morris <amorris@mistermorris.com>
2017-02-09 15:21:11 +0800

    relative import

--- synctree/templates/__init__.py
+++ synctree/templates/__init__.py
@@ -1,5 +1,6 @@
 from .default_template import DefaultTemplate
 from .logger_template import LoggerTemplate
 from .logger_template import LoggerReporter
+from .logger_template import PrintTemplate
 
-__all__ = [DefaultTemplate, LoggerTemplate, LoggerReporter]
+__all__ = [DefaultTemplate, PrintTemplate, LoggerTemplate, LoggerReporter]
